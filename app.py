ebd9e2cfcefaf2c50736deed920dfb7e5e338331
lucas.marten <lucas.marten@climatempo.com.br>
2022-10-02 16:44:20 -0300

    update: data_lake

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pandas import date_range, to_datetime, DataFrame
 import helpers
 from bs4 import BeautifulSoup
 import os
-
+import time
 
 CFG = helpers.load_cfg()
 FMT = '%Y%m%d%H'
@@ -21,75 +21,55 @@ def get_new_tab(driver, date_obj):
     return 
 
 def gen_list_dates(start, end):
-    return [to_datetime(d) for d in date_range(start, end, freq='12H')]
+    return [to_datetime(d) for d in date_range(start, end, freq='6H')]
 
-
-def create_response_lists():
-    dates = list()
-    text_data = list()
-    url_data = list()
-    level_data = list()
-    prev24 = list()
-    prev48 = list()
-    prev72 = list()
-    prev96 = list()
-    prev120 = list()
-    return dates, text_data, url_data, level_data, prev24, prev48, prev72, prev96, prev120
-
-def append_response_lists(dates, text_data, url_data, level_data, prev24, prev48, prev72, prev96, prev120):
-    dates.append(date_rod)
-    text_data.append(text)
-    url_data.append(url)
-    level_data.append(level)
-    prev24.append(soup.find_all('a')[-1].get_attribute_list('href')[0].split("'")[1])
-    prev48.append(soup.find_all('a')[-2].get_attribute_list('href')[0].split("'")[1])
-    prev72.append(soup.find_all('a')[-3].get_attribute_list('href')[0].split("'")[1])
-    prev96.append(soup.find_all('a')[-4].get_attribute_list('href')[0].split("'")[1])
-    prev120.append(soup.find_all('a')[-5].get_attribute_list('href')[0].split("'")[1])
-    return dates, text_data, url_data, level_data, prev24, prev48, prev72, prev96, prev120
-
-def make_response(dates, text_data, url_data, level_data, prev24, prev48, prev72, prev96, prev120):
-    return {
-            "dates":dates,
-            "text_data":text_data,
-            "url_data":url_data,
-            "level_data":level_data,
-            "prev24":prev24,
-            "prev48":prev48,
-            "prev72":prev72,
-            "prev96":prev96,
-            "prev120":prev120
-    }
 
 if __name__ == "__main__":
         
-    for date_obj in gen_list_dates('20090101T00', '20091231T12'):
-        
-        date_rod = date_obj.strftime(FMT)
-        home_tab = helpers.get_current_tab(driver=driver)
-        get_new_tab(driver, date_obj)
+    for date_obj in gen_list_dates('20090101T00', '20090103T00'):
 
-        if home_tab == helpers.get_current_tab(driver=driver):
-            helpers.switch_to_tab_by_index(driver, driver.window_handles[-1])
+        if date_obj.strftime('%H') in ["00", "12", "18"]:
+            date_rod = date_obj.strftime(FMT)
+            home_tab = helpers.get_current_tab(driver=driver)
+            get_new_tab(driver, date_obj)
 
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        texts = soup.find_all(class_="texto")
-        dates, text_data, url_data, level_data, prev24, prev48, prev72, prev96, prev120 = create_response_lists()
-        
-        for i, data in enumerate(texts):
+            if home_tab == helpers.get_current_tab(driver=driver):
+                helpers.switch_to_tab_by_index(driver, driver.window_handles[-1])
+
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+            texts = soup.find_all(class_="texto")
+
+            dates = list()
+            text_data = list()
+            url_data = list()
+            level_data = list()
+            prev24 = list()
+            prev48 = list()
+            prev72 = list()
+            prev96 = list()
+            prev120 = list()
             
-            if data.find('a'):
-                level = CFG['structure_chart'][i]
-                url = data.find('a').get_attribute_list('href')[0].split("'")[1]
-                text = data.text
-                append_response_lists(dates, text_data, url_data, level_data, prev24, prev48, prev72, prev96, prev120)
+            for i, data in enumerate(texts):
+                
+                if data.find('a'):
+                    level = CFG['structure_chart'][i]
+                    url = data.find('a').get_attribute_list('href')[0].split("'")[1]
+                    text = data.text
 
-        os.system(f"mkdir -p ./data/{date_obj.strftime('%Y')}/")
-        
-        response = make_response(dates, text_data, url_data, level_data, prev24, prev48, prev72, prev96, prev120)
-        
-        DataFrame(response).to_csv(f'./data/{date_obj.strftime("%Y")}/{date_rod}.csv')
-
-        helpers.switch_to_tab_by_index(driver, driver.window_handles[0])
-
+                    os.system(f"mkdir -p ./data/{date_obj.strftime('%Y')}/{level}")
+                    
+                    resp = dict()
+                    resp["dates"] = [date_rod]
+                    resp["text_data"] = [text]
+                    resp["url_data"] = [url]
+                    resp["prev24"] = [soup.find_all('a')[-1].get_attribute_list('href')[0].split("'")[1]]
+                    resp["prev48"] = [soup.find_all('a')[-2].get_attribute_list('href')[0].split("'")[1]]
+                    resp["prev72"] = [soup.find_all('a')[-3].get_attribute_list('href')[0].split("'")[1]]
+                    resp["prev96"] = [soup.find_all('a')[-4].get_attribute_list('href')[0].split("'")[1]]
+                    resp["prev120"] = [soup.find_all('a')[-5].get_attribute_list('href')[0].split("'")[1]]
+                    print(resp)
+            
+                    DataFrame(resp).to_csv(f'./data/{date_obj.strftime("%Y")}/{level}/{level}_{date_rod}.csv')
+            time.sleep(3)
+            helpers.switch_to_tab_by_index(driver, driver.window_handles[0])
